fd4a47daa02652a0227071ab64a3828f54000934
David  Penedo <131776809+Penedodp@users.noreply.github.com>
2023-10-20 09:56:53 +0200

    secction-detail

---
+++ Front/src/app/main/sectionfood/sectionfood-detail/sectionfood-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sectionfood-detail',
+  templateUrl: './sectionfood-detail.component.html',
+  styleUrls: ['./sectionfood-detail.component.css']
+})
+export class SectionfoodDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
