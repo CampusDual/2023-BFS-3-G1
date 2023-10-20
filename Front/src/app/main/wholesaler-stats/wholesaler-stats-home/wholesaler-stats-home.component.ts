e62f2fb7f0af9048a5e7cbd3f350abd78bcbeede
Alfredo Diaz <131871192+ulises-ajdd@users.noreply.github.com>
2023-10-20 13:51:52 +0200

    graficas

--- Front/src/app/main/wholesaler-stats/wholesaler-stats-home/wholesaler-stats-home.component.ts
+++ Front/src/app/main/wholesaler-stats/wholesaler-stats-home/wholesaler-stats-home.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ChartSeries, PieChartConfiguration } from "ontimize-web-ngx-charts";
+import { D3LocaleService } from "../../../shared/d3-locale/d3Locale.service";
 
 @Component({
-  selector: 'app-wholesaler-stats-home',
-  templateUrl: './wholesaler-stats-home.component.html',
-  styleUrls: ['./wholesaler-stats-home.component.css']
+  selector: "app-wholesaler-stats-home",
+  templateUrl: "./wholesaler-stats-home.component.html",
+  styleUrls: ["./wholesaler-stats-home.component.css"],
 })
 export class WholesalerStatsHomeComponent implements OnInit {
+  public movementTypesChartParams: PieChartConfiguration;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private d3LocaleService: D3LocaleService) {
+    const d3Locale = this.d3LocaleService.getD3LocaleConfiguration();
+    this._configurePieChart(d3Locale);
   }
 
+  ngOnInit() {}
+
+  private _configurePieChart(locale: any): void {
+    this.movementTypesChartParams = new PieChartConfiguration();
+    this.movementTypesChartParams.margin.top = 0;
+    this.movementTypesChartParams.margin.right = 0;
+    this.movementTypesChartParams.margin.bottom = 0;
+    this.movementTypesChartParams.margin.left = 0;
+    this.movementTypesChartParams.legendPosition = "bottom";
+    this.movementTypesChartParams.legend.vers = "furious";
+    this.movementTypesChartParams.labelType = "value";
+    this.movementTypesChartParams.valueType = locale.numberFormat("$,.2f");
+  }
 }
